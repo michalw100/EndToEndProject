24719ec607bf17127e43645b4e6dd7d99d6ee488
chani86370 <chani86370@gmail.com>
2024-05-01 20:24:25 +0300

    add posts controller

---
+++ controllers/postsController.js
@@ -0,0 +1,42 @@
+const model = require('../models/postsModel');
+
+async function create(title, body, userID) {
+    try {
+        return model.createPost(title, body, userID);
+    } catch (err) {
+        throw err;
+    }
+
+}
+async function getAll() {
+    try {
+        return model.getPosts();
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function getById(id) {
+    try {
+        return model.getPost(id);
+    } catch (err) {
+        throw err;
+    }
+}
+async function deleteById(id){
+    try {
+        return model.deletePost(id);
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function update(id,title, body, userID) {
+    try {
+        return model.updatePost(id,title, body, userID);
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports = { create, getAll, getById,deleteById,update }
