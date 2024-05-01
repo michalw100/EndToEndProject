3bed1c55192f22fb6f000c723acc4440c5dcf2a2
chani86370 <chani86370@gmail.com>
2024-05-01 19:51:20 +0300

    install npm libraris

--- DB.js
+++ DB.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: 'toy-store',
+  database: 'postsdb',
   port: 3306,
   password: 'jbhkuh',
 }).promise();
